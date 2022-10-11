1f3da45a19de33ab053b4db765133cacbdf42c9c
AlexandrSawrasov <caiiiuk@mail.ru>
2022-10-12 00:44:22 +0300

    add Examples005 IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+    Console.WriteLine("!! А где медведь?");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+    Console.WriteLine("!!");
+}
