80b47e682f608a4997c690b68eeb183aca18752e
AlexandrSawrasov <caiiiuk@mail.ru>
2022-10-12 00:18:26 +0300

    add Example 003 random

--- Exemple003_Sum/Program.cs
+++ Exemple003_Sum/Program.cs
@@ -1,4 +1,8 @@
-﻿int nanberA = 33;
-int nanberB = 52;
+﻿int nanberA = new Random().Next(1, 10); // 1 2 3 ... 9
+Console.Write(nanberA);
+int nanberB = new Random().Next(1, 10);
+Console.Write(" + ");
+Console.Write(nanberB);
 int result = nanberA + nanberB;
+Console.Write(" = ");
 Console.WriteLine(result);
