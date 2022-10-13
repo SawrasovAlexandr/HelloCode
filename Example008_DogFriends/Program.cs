e5b0fd6180c34170bd3349cdbaa81f5cf99b217a
AlexandrSawrasov <caiiiuk@mail.ru>
2022-10-13 18:09:52 +0300

    Mind & adequecy check 008

--- Example008_DogFriends/Program.cs
+++ Example008_DogFriends/Program.cs
@@ -1,15 +1,15 @@
 ﻿Console.Clear();
 Console.WriteLine();                                //отделяем вывод данных программы пустой строкой
-Console.Write("Введите изначальное расстояние между друзьями в метрах: ");   //запрашиваем и
-string dis = Console.ReadLine();                      //принимаем расстояние
-double distance = 0;
-while (double.TryParse(dis, out distance) == false | distance <= 0)
-{
-    int answer = new Random().Next(0, 4);
-    if (answer == 0)
-    {
+Console.Write("Введите изначальное расстояние между друзьями в метрах: "); //запрашиваем и
+string dis = Console.ReadLine();                                           //принимаем расстояние
+double distance = 0;                                        //определяем переменную для расстояния
+while (double.TryParse(dis, out distance) == false | distance <= 0) //если введено не числовое
+{                                                                   //или отрицательное значение
+    int answer = new Random().Next(0, 4);                 //создаем рандомизированную переменную
+    if (answer == 0)                                      //и в зависимости от ее значения выводим 
+    {                                                     //разные сообщения
         Console.Write("Расстояние это натуральное число! Введите снова: ");
-        dis = Console.ReadLine();
+        dis = Console.ReadLine();                         //и просим повторить ввод
     }
     if (answer == 1)
     {
@@ -26,50 +26,70 @@ while (double.TryParse(dis, out distance) == false | distance <= 0)
         Console.Write("Ошибка! Введите снова: ");
         dis = Console.ReadLine();
     }
-}
+}                                               //если все же введено положительное число
 while (distance > 40075000)                                             //проверяем не слишком ли
 {                                                                       //оно велико и если это так
     Console.Write("Вообще то экватор короче! Введите число поменьше: ");//просим повторить ввод и
-    distance = Convert.ToDouble(Console.ReadLine());                    //обновляем значение
+    dis = Console.ReadLine();                                           //обновляем значение
+    while (double.TryParse(dis, out distance) == false | distance <= 0)     //при этом снова проверяем 
+    {                                                       //корректность ввода
+        Console.Write("Ошибка! Введите снова: ");
+        dis = Console.ReadLine();
+    }                                                                       
 }
-Console.Write("Введите скорость хозяина собаки в м/с: ");       //запрашиваем и 
-string ffs = Console.ReadLine();                      //принимаем расстояние
+Console.Write("Введите скорость хозяина собаки в м/с: ");       
+string ffs = Console.ReadLine();                                
 double firstFrindSpeed = 0;
 while (double.TryParse(ffs, out firstFrindSpeed) == false | firstFrindSpeed <= 0)
 {                              
     Console.Write("Ошибка ввода! Введите снова: ");
     ffs = Console.ReadLine();
-}                                       //принимаем скорость хозяина собаки
-while (firstFrindSpeed > 12.4)                                              //проверяем не слишком ли
-{                                                                           //оно велико и если это так
-    Console.Write("Усейн Болт медленней бегает! Введите число поменьше: "); //просим повторить ввод и
-    firstFrindSpeed = Convert.ToDouble(Console.ReadLine());                 //обновляем значение 
+}                                       
+while (firstFrindSpeed > 12.4)                                              
+{                                                                           
+    Console.Write("Усейн Болт медленней бегает! Введите число поменьше: "); 
+    ffs = Console.ReadLine();
+    while (double.TryParse(ffs, out firstFrindSpeed) == false | firstFrindSpeed <= 0)
+    {
+        Console.Write("Ошибка! Введите снова: ");
+        ffs = Console.ReadLine();
+    }                     
 }
-Console.Write("Введите скорость его друга в м/с: ");            //запрашиваем и 
-string sfs = Console.ReadLine();                      //принимаем расстояние
+Console.Write("Введите скорость его друга в м/с: ");           
+string sfs = Console.ReadLine();                     
 double secondFrindSpeed = 0;
 while (double.TryParse(sfs, out secondFrindSpeed) == false | secondFrindSpeed <= 0)
 {                              
     Console.Write("Ошибка ввода! Введите снова: ");
     sfs = Console.ReadLine();
-}                                                            //принимаем скорость его друга
-while (secondFrindSpeed > 12.4)                                             //проверяем не слишком ли
-{                                                                           //оно велико и если это так
-    Console.Write("Усейн Болт медленней бегает! Введите число поменьше: "); //просим повторить ввод и
-    secondFrindSpeed = Convert.ToDouble(Console.ReadLine());                //обновляем значение 
+}                                                            
+while (secondFrindSpeed > 12.4)                                            
+{                                                                           
+    Console.Write("Усейн Болт медленней бегает! Введите число поменьше: "); 
+    sfs = Console.ReadLine();
+    while (double.TryParse(sfs, out secondFrindSpeed) == false | secondFrindSpeed <= 0)
+    {
+        Console.Write("Ошибка! Введите снова: ");
+        sfs = Console.ReadLine();
+    }                          
 }
-Console.Write("Введите скорость собаки в м/с: ");           //запрашиваем и 
-string ds = Console.ReadLine();                      //принимаем расстояние
+Console.Write("Введите скорость собаки в м/с: ");          
+string ds = Console.ReadLine();                      
 double dogSpeed = 0;
 while (double.TryParse(ds, out dogSpeed) == false | dogSpeed <= 0)
 {                              
     Console.Write("Ошибка ввода! Введите снова: ");
     ds = Console.ReadLine();
-}                                     //принимаем скорость собаки
-while (dogSpeed > 18.6)                                                     //проверяем не слишком ли
-{                                                                           //оно велико и если это так
-    Console.Write("Стар Тайтл медленней бегает! Введите число поменьше: "); //просим повторить ввод и
-    dogSpeed = Convert.ToDouble(Console.ReadLine());                        //обновляем значение
+}                                     
+while (dogSpeed > 18.6)                                                   
+{                                                                
+    Console.Write("Стар Тайтл медленней бегает! Введите число поменьше: "); 
+    ds = Console.ReadLine();
+    while (double.TryParse(ds, out dogSpeed) == false | dogSpeed <= 0)
+    {
+        Console.Write("Ошибка! Введите снова: ");
+        ds = Console.ReadLine();
+    }                                   
 }
 byte friend = 2;    //задаем счетчик направления бега собаки
 int count = 0;      //задаем счетчик количества пробежек собаки
